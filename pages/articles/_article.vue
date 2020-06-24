ba301ab6084032be5057bccad90172fe80ee2b31
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2020-06-24 20:59:42 +0200

    feat(Firebase): Added firebase

--- pages/articles/_article.vue
+++ pages/articles/_article.vue
@@ -1,11 +1,6 @@
-<template>
-	<div>
-		<nuxt-content :document="article" />
-	</div>
-</template>
-
 <script lang="ts">
 	import Vue from "vue";
+
 	export default Vue.extend({
 		async asyncData({ params, $content }) {
 			const article = await $content("articles", params.article).fetch();
@@ -16,5 +11,11 @@
 		},
 	});
 </script>
+
+<template>
+	<div class="container max-w-screen-sm md:max-w-screen-md mx-auto pl-4 pr-4 md:p-0">
+		<nuxt-content :document="article" />
+	</div>
+</template>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
 
 <template>
 	<div class="container max-w-screen-sm md:max-w-screen-md mx-auto pl-4 pr-4 md:p-0">
-		<nuxt-content :document="index" />
+		<div></div>
 	</div>
 </template>
 
