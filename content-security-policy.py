597c4b5c1ba0392692d42db1595a39c76f289631
Torbjørn <torbjornholtmon@gmail.com>
2020-08-21 16:13:25 +0200

    feat(Firebase): Test firebase headers

--- content-security-policy.py
+++ content-security-policy.py
@@ -1,4 +1,11 @@
 import os
+import json
+
+# parses the existing json into a dictionary
+with open('firebase.json') as firebase_json:
+    firebase_json_dict = json.loads(firebase_json)
+
+
 for root, dirs, files in os.walk("./dist"):
     for file in files:
         if file.endswith(".html"):
