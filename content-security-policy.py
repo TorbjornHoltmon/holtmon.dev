86a27099d8a1a660532c352e113c320db78059f9
Torbjørn <torbjornholtmon@gmail.com>
2020-08-23 22:13:17 +0200

    feat(csp-python): finished v1 of python script

--- content-security-policy.py
+++ content-security-policy.py
@@ -30,8 +30,8 @@ def get_sha256_base64_of_scripts(path_to_html_file: str):
     return hashes
 
 
-def add_page_csp(base_csp_input, hashes: []):
-    new_csp: dict = copy.copy(base_csp_input)
+def add_root_csp(base_csp_input, hashes: []):
+    new_csp: dict = copy.deepcopy(base_csp_input)
     script_hash: str
     for script_hash in hashes:
         new_csp_value: str = new_csp["value"]
@@ -41,27 +41,41 @@ def add_page_csp(base_csp_input, hashes: []):
     return new_csp
 
 
+def add_page_csp(base_csp_config, hashes: [], source: str):
+    new_csp: dict = copy.deepcopy(base_csp_config)
+    source_path = source.replace(
+        "./dist", "").replace("/index.html", "")
+    new_csp["source"] = source_path
+    for script_hash in hashes:
+        new_csp_value: str = new_csp["headers"][5]["value"]
+        sub_string = "script-src 'self'"
+        new_csp["headers"][5]["value"] = new_csp_value.replace(
+            sub_string, sub_string + " 'sha256-" + script_hash + "'")
+    return new_csp
+
+
 # parses the existing json into a dictionary
 with open('firebase.json', "r") as firebase_json:
     firebase_json_dict = json.load(firebase_json)
 firebase_json.close()
 
-base_csp = firebase_json_dict["hosting"]["headers"][0]["headers"][5]
+base_config = copy.deepcopy(firebase_json_dict["hosting"]["headers"][0])
+base_csp = copy.deepcopy(
+    firebase_json_dict["hosting"]["headers"][0]["headers"][5])
 
 
 for root, dirs, files in os.walk("./dist"):
     for file in files:
         file_path = os.path.join(root, file)
-        if file.endswith("404.html"):
+        if file_path == "./dist/index.html":
             hashes = get_sha256_base64_of_scripts(file_path)
             firebase_json_dict["hosting"]["headers"][0]["headers"][5] \
-                = add_page_csp(base_csp, hashes)
+                = add_root_csp(base_csp, hashes)
         elif file.endswith(".html"):
             hashes = get_sha256_base64_of_scripts(file_path)
             firebase_json_dict["hosting"]["headers"].append(
-                add_page_csp(base_csp, hashes))
-            print(firebase_json_dict)
+                add_page_csp(base_config, hashes, file_path))
 
-        # elif os.path.join(root, file) == "./dist/index.html":
-        #     print("root")
-        #     print(os.path.join(root, file))
+
+with open('firebase.json', 'w') as outfile:
+    json.dump(firebase_json_dict, outfile)
