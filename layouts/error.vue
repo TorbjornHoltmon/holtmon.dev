7b3418c4b0f29b41617dbafc75932c67bb391261
Torbjørn <torbjornholtmon@gmail.com>
2020-08-23 23:56:16 +0200

    feat(404): added 404 page again

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Vue from "vue";
+  export default Vue.extend({
+    // eslint-disable-next-line vue/require-prop-types
+    props: ["error"],
+  });
+</script>
+
+<template>
+  <div class="bg-background w-screen flex flex-col h-screen justify-center content-center">
+    <template v-if="error.statusCode === 404">
+      <h1 class="text-center">404</h1>
+      <nuxt-link class="flex justify-center" to="/"
+        ><img src="/images/404.gif" class="object-contain"
+      /></nuxt-link>
+      <h1 class="text-center">
+        <nuxt-link to="/">Back to home</nuxt-link>
+      </h1>
+    </template>
+    <template v-else>
+      <h1 class="text-center">something really bad happen...</h1>
+      <h1 class="text-center">
+        <nuxt-link to="/">Back to home</nuxt-link>
+      </h1>
+    </template>
+  </div>
+</template>
