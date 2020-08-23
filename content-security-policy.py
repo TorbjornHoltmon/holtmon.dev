bedd2d44bf7dc4cf68050ff2fecdc7fef95a39f7
Torbjørn <torbjornholtmon@gmail.com>
2020-08-23 08:13:01 +0200

    feat(Firebase): 404 header test

--- content-security-policy.py
+++ content-security-policy.py
@@ -1,30 +1,50 @@
 import os
 import json
+import hashlib
+import base64
+import copy
 from bs4 import BeautifulSoup
 
 
-def get_text_from_soup(soup):
+def get_text_from_soup(soup: BeautifulSoup):
     scripts = []
-    for script in soup:
-        print(script.contents)
-        scripts.append(script.contents)
+    scriptSoup: BeautifulSoup
+    for scriptSoup in soup:
+        scripts.append(scriptSoup.contents[0])
     return scripts
 
 
-def get_scripts_from_html(path_to_html_file):
+def get_scripts_from_html(path_to_html_file: str):
     with open(path_to_html_file, 'r') as file:
         soup = BeautifulSoup(file.read(), features='html.parser')
         scripts = soup.find_all('script', src=False)
-        print(get_text_from_soup(scripts))
+        return get_text_from_soup(scripts)
 
 
-def get_sha256_base64_from_string():
-    return "string"
+def get_sha256_base64_of_scripts(path_to_html_file: str):
+    hashes = []
+    for script in get_scripts_from_html(path_to_html_file):
+        hashed_script = base64.b64encode(
+            hashlib.sha256(script.encode('utf-8')).digest())
+        hashes.append(hashed_script.decode("utf-8"))
+    return hashes
+
+
+def add_page_csp(base_csp_input, hashes: []):
+    new_csp: dict = copy.copy(base_csp_input)
+    script_hash: str
+    for script_hash in hashes:
+        new_csp_value: str = new_csp["value"]
+        sub_string = "script-src 'self'"
+        new_csp["value"] = new_csp_value.replace(
+            sub_string, sub_string + " 'sha256-" + script_hash + "'")
+    return new_csp
 
 
 # parses the existing json into a dictionary
-with open('firebase.json') as firebase_json:
+with open('firebase.json', "r") as firebase_json:
     firebase_json_dict = json.load(firebase_json)
+firebase_json.close()
 
 base_csp = firebase_json_dict["hosting"]["headers"][0]["headers"][5]
 
@@ -33,12 +53,15 @@ for root, dirs, files in os.walk("./dist"):
     for file in files:
         file_path = os.path.join(root, file)
         if file.endswith("404.html"):
-            print("404")
-            print(get_scripts_from_html(file_path))
-        # elif file.endswith(".html") \
-        #         & (os.path.join(root, file) != "./dist/index.html"):
-        #     print("anything else")
-        #     print(os.path.join(root, file))
+            hashes = get_sha256_base64_of_scripts(file_path)
+            firebase_json_dict["hosting"]["headers"][0]["headers"][5] \
+                = add_page_csp(base_csp, hashes)
+        elif file.endswith(".html"):
+            hashes = get_sha256_base64_of_scripts(file_path)
+            firebase_json_dict["hosting"]["headers"].append(
+                add_page_csp(base_csp, hashes))
+            print(add_page_csp(base_csp, hashes))
+
         # elif os.path.join(root, file) == "./dist/index.html":
         #     print("root")
         #     print(os.path.join(root, file))
