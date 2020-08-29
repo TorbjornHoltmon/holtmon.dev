798505c0a7aeb59e490314193d2276c65e4ca36c
Torbjørn <torbjornholtmon@gmail.com>
2020-08-29 17:13:17 +0200

    feat(articles, packages): updated packages, updated headers

--- pages/articles/_article.vue
+++ pages/articles/_article.vue
@@ -1,19 +1,25 @@
-<script lang="ts">
-  import Vue from "vue";
-
-  export default Vue.extend({
+<script>
+  export default {
     async asyncData({ params, $content }) {
       const article = await $content("articles", params.article).fetch();
-
       return {
         article,
       };
     },
-  });
+    head() {
+      return {
+        title: (this.article.seo_title || this.article.title) + " | Torbjørn Holtmon",
+        meta: [
+          { hid: "description", name: "description", content: this.article.seo_desc || this.article.preview },
+        ],
+      };
+    },
+  };
 </script>
 
 <template>
   <div class="container max-w-screen-sm md:max-w-screen-md mx-auto pl-4 pr-4 md:p-0">
+    <h1>{{ article.title }}</h1>
     <nuxt-content :document="article" />
   </div>
 </template>
