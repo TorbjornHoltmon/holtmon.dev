1c2435547a3eba90cb15e824756baa8ed430c8ae
Torbjørn <torbjornholtmon@gmail.com>
2020-08-30 07:23:58 +0200

    feat(articles): Removed poster image from list

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   export default Vue.extend({
     async asyncData({ $content }) {
       const articles = await $content("articles")
-        .only(["title", "description", "slug", "date", "poster_image"])
+        .only(["title", "description", "slug", "date"])
         .sortBy("date", "description")
         .fetch();
 
@@ -27,20 +27,14 @@
 <template>
   <div class="container max-w-screen-sm md:max-w-screen-md mx-auto">
     <h1 class="text-left">Articles</h1>
-    <hr />
-    <div v-for="article of articles" :key="article.slug" class="mt-10">
+    <hr class="mb-10" />
+    <div v-for="article of articles" :key="article.slug">
       <h5>
         <NuxtLink :to="{ name: 'articles-article', params: { article: article.slug } }">
           {{ article.title }}
         </NuxtLink>
       </h5>
-      <p>
-        <em>Last updated: {{ articleDate(article.date) }}</em>
-      </p>
-      <NuxtLink :to="{ name: 'articles-article', params: { article: article.slug } }">
-        <img class="object-cover h-48 mb-10 w-full" :src="article.poster_image" alt="Article header image" />
-      </NuxtLink>
-      <p>{{ article.description }}</p>
+      <em>Last updated: {{ articleDate(article.date) }}</em>
     </div>
   </div>
 </template>
