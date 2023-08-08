6b33c227b5a80dcf2736da5ee52b0ce6675b7660
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2023-08-08 21:48:12 +0200

    feat: added ci

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 export default defineNuxtConfig({
+  nitro: {
+    preset: "cloudflare-pages",
+  },
   // https://github.com/nuxt-themes/alpine
-  extends: '@nuxt-themes/alpine',
+  extends: "@nuxt-themes/alpine",
 
   modules: [
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
+    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
-})
+    "@nuxt/devtools",
+    "@nuxthq/studio",
+  ],
+});
