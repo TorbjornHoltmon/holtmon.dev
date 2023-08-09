136c60ba0753afbd98ac44cd64e0e923008707ea
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2023-08-09 19:48:55 +0200

    feat: use edge channel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,6 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: "@nuxt-themes/alpine",
   modules: [
-    // https://github.com/nuxt-modules/plausible
-    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
     "@nuxt/devtools",
     "@nuxthq/studio",
